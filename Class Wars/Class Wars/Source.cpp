96d4331f996b11451a433ff76406143b699cb642
AndrewHarsh <harsh@purdue.edu>
2016-02-28 01:33:14 -0500

    Changed some stuff

--- Class Wars/Class Wars/Source.cpp
+++ Class Wars/Class Wars/Source.cpp
@@ -4,22 +4,45 @@
 using namespace SLGE;
 const int ARRAY_SIZE = 10;
 
-Window_t Window1(640, 480, "Cooler Program", false);
 
+//
+//  Define your classes	(Can be in separate header or dll)
+//
 
-class Player_TT : public Object_t
+class AI_t : public Object_t
 {
-	friend class DynamicClass <Player_TT>;
+public:
+
+	AI_t()
+	{
+	}
+
+	AI_t(Window_t *in_Window)
+	{
+		WindowHandle = in_Window;
+		OpenImage("1.png", { 0, 0, 32, 32 }, { 255, 0, 153, 0 });
+	}
+};
+
+class Player_t : public Object_t
+{
+	friend class DynamicClass <Player_t>;
 
 public:
 
-	Player_TT()
+	Player_t()
 	{
 	};
 
-	Player_TT(Window_t *in_Window)
+	Player_t(Window_t *in_Window)
 	{
 		WindowHandle = in_Window;
+
+		for (int i = 0; i < 20; i++)
+		{
+			for (int ii = 0; ii < 8; ii++)
+				OpenImage("Player.png", { (ii * 100) + 1, (i * 100) + 1, 98, 98 }, { 255, 255, 255, 0 });
+		}
 	}
 
 	void EventHandler()
@@ -41,58 +64,62 @@ public:
 	}
 };
 
+class Other_t : public Player_t
+{
+public:
+
+	Other_t()
+	{
+	};
+
+	Other_t(Window_t *in_Window)
+	{
+		WindowHandle = in_Window;
+		OpenImage("1.png", { 0, 0, 32, 32 }, { 255, 0, 153, 0 });
+	}
+};
+
+
+//
+//	Declare your classes and window (Can be in separate header or dll but must come after classes are defined)
+//
+
+Window_t Window1(640, 480, "Cooler Program", false);
 
 DynamicClass <Object_t> Background;
-DynamicClass <Object_t> Array;
-DynamicClass <Player_TT, Object_t> Player;
+DynamicClass <AI_t, Object_t> Array;
+DynamicClass <Player_t, Object_t> Player;
+DynamicClass <Other_t, Player_t, Object_t> Other;
 
-
-
+//
+//  Declare the game init, loop, and despawn code (Can be in separate header or dll but must come after classes are initialized)
+//
 
 void SpawnGame()
 {
-	Background.Spawn(1, Window1);
-	Array.Spawn(10, Window1);
-	//Player.template Spawn <Player_TT> (1, Window1);
-	Player.Spawn(1, Window1);
-	//Other.Spawn(1, Window1);
+	//Spawn
+	Background.Spawn(1, &Window1);
+	Array.Spawn(10, &Window1);
+	Player.Spawn(2, &Window1);
+	Other.Spawn(5, &Window1);
 
+	//Set the objects in their place
 	Background[0].OpenImage("Image.png", { NULL }, { NULL });
 	Background[0].SetCoords(0, 0, Window1.GetWidth(), Window1.GetHeight());
 
-	for (int i = 0; i < 10; i++)
-	{
-		Array[i].OpenImage("1.png", { 0, 0, 32, 32 }, { 255, 0, 153, 0 });
+	for (int i = 0; i < Array.NumberOfObjects(); i++)
 		Array[i].SetCoords(i * 50, 240, 32, 32);
-	}
-
-	for (int i = 0; i < 20; i++)
-	{
-		for (int ii = 0; ii < 8; ii++)
-			Player[0].OpenImage("Player.png", { (ii * 100) + 1, (i * 100) + 1, 98, 98 }, { 255, 255, 255, 0 });
-	}
-
-	for (int i = 0; i < 20; i++)
-	{
-		for (int ii = 0; ii < 8; ii++)
-			Player[1].OpenImage("Player.png", { (ii * 100) + 1, (i * 100) + 1, 98, 98 }, { 255, 255, 255, 0 });
-	}
 
 	Player[0].SetCoords(300, 500, 32, 32);
 	Player[1].SetCoords(100, 100, 32, 32);
+
+	for (int i = 0; i < Other.NumberOfObjects(); i++)
+		Other[i].SetCoords(i * 100 + 100, 400, 32, 32);
 }
 
 void RunGame()
 {
-	//std::vector <Object_t> Array1;
-
-	//for (int i = 0; i < Array1.size(); i++)
-	//	Array[i].Display();
-
-
-
-	//Player[0].EventHandler();
-	DynamicClass <Player_TT>::All(&Player_TT::EventHandler);
+	DynamicClass <Player_t>::All(&Player_t::EventHandler);
 
 	DynamicClass <Object_t>::All(&Object_t::Display);
 }
@@ -102,13 +129,19 @@ void DespawnGame()
 	Background.Despawn();
 	Array.Despawn();
 	Player.Despawn();
+	Other.Despawn();
 }
 
+
+//
+//  Main program loop will look simple
+//
 
 extern "C" int SDL_main(int argc, char* argv[])
 {
 	srand(unsigned(time(NULL)));
 
+	//Do Window1.Run(init, loop, despawn) on any new screen
 	Window1.Run(SpawnGame, RunGame, DespawnGame);
 
 	return 0;
--- Class Wars/Game Engine/SLGE.h
+++ Class Wars/Game Engine/SLGE.h
@@ -107,60 +107,31 @@ namespace SLGE
 	///vvvvvvvvvvvvvvv///
 
 
-	struct LinkPtr
+	struct Local
 	{
 		int Data;
-		int Pointer;
+		int Index;
+	};
+
+	struct Global
+	{
+		std::vector <Local> *Pointer;
+		int Index;
 	};
 
 	//Used to consolidate the class lists between derived and base classes
 	template <class C>
-	class Data_t
+	struct Data_t
 	{
-		//template <typename T>
-		//friend class DynamicClass;
-	public:
 		static std::vector <C*> Data;
-		static std::vector <int> Index;
-		static std::vector <LinkPtr*> Link;
-
-		static void Add()
-		{
-
-		}
-
-		static void Swap(int Pos1, int Pos2)
-		{
-			if (Pos1 >= 0 && Pos2 >= 0 && Pos1 < Data.size() && Pos2 < Data.size())
-			{
-				C* Temp = Data[Pos2];
-				LinkPtr* TempPtr = Link[Pos2];
-
-				Data[Pos2] = Data[Pos1];
-				Data[Pos1] = Temp;
-
-				Link[Pos2]->Index = Pos1;
-				Link[Pos1]->Index = Pos2;
-
-				Link[Pos2]->Pointer = Pos1;
-				Link[Pos1]->Pointer = Pos2;
-			}
-		}
-
-		static void Remove()
-		{
-
-		}
+		static std::vector <Global> Link;
 	};
 
 	template <class C> 
 	std::vector <C*> Data_t <C>::Data;
 
 	template <class C> 
-	std::vector <int> Data_t <C>::Index;
-
-	template <class C> 
-	std::vector <LinkPtr*> Data_t <C>::Link;
+	std::vector <Global> Data_t <C>::Link;
 
 
 	template <typename C, typename ...D>
@@ -172,50 +143,63 @@ namespace SLGE
 		const int NUMBER_OF_TYPE_ARGS = sizeof...(D) + 1;
 
 		const std::vector <C*> *DATA = &(Data_t <C>::Data);
-		const std::vector <int> *INDEX = &(Data_t <C>::Index);
-		const std::vector <LinkPtr*> *LINK = &(Data_t <C>::Link);
+		const std::vector <Global> *LINK = &(Data_t <C>::Link);
 
 
 		std::vector <C*> Data;
-		std::vector <LinkPtr> Index[sizeof...(D) + 1];
+		std::vector <Local> Link[sizeof...(D) + 1];
 
 
 		template <typename T1>
 		void AddPointer()
 		{
 			const int TYPE_NUM = 0;
+			Local TempL;
+			Global TempG;
 
-			//Add pointer to data vector and data vector index
+			// Point to the data
 			Data_t <T1>::Data.push_back(Data[Data.size() - 1]);
-			Data_t <T1>::Index.push_back(Data.size() - 1);
 
-			//Incease Index and put the index of the data pointer in Index 
-			Index[TYPE_NUM].push_back({Data_t <T1>::Data.size() - 1, 0});
+			// Local
+			TempL.Data = Data_t <T1>::Data.size() - 1;
+			TempL.Index = 0;
 
-			//Point Link to the address of Index
-			Data_t <T1>::Link.push_back(&Index[TYPE_NUM][Index[TYPE_NUM].size() - 1]);
+			Link[TYPE_NUM].push_back(TempL);
 
-			//Put the index of the link vector in Index
-			Index[TYPE_NUM][Index[TYPE_NUM].size() - 1].Pointer = Data_t <T1>::Link.size() - 1;
+			// Global
+			TempG.Pointer = &Link[TYPE_NUM];
+			TempG.Index = Link[TYPE_NUM].size() - 1;
+
+			Data_t <T1>::Link.push_back(TempG);
+
+			// Update Local
+			Link[TYPE_NUM][Link[TYPE_NUM].size() - 1].Index = Data_t <T1>::Link.size() - 1;
 		}
 
 		template <typename T1, typename T2, typename ...Args>
 		void AddPointer()
 		{
 			const int TYPE_NUM = sizeof...(Args) + 1;
+			Local TempL;
+			Global TempG;
 
-			//Add pointer to data vector and data vector index
+			// Point to the data
 			Data_t <T1>::Data.push_back(Data[Data.size() - 1]);
-			Data_t <T1>::Index.push_back(Data.size() - 1);
 
-			//Incease Index and put the index of the data pointer in Index 
-			Index[TYPE_NUM].push_back({Data_t <T1>::Data.size() - 1, 0});
+			// Local
+			TempL.Data = Data_t <T1>::Data.size() - 1;
+			TempL.Index = 0;
 
-			//Point Link to the address of Index
-			Data_t <T1>::Link.push_back(&Index[TYPE_NUM][Index[TYPE_NUM].size() - 1]);
+			Link[TYPE_NUM].push_back(TempL);
 
-			//Put the index of the link vector in Index
-			Index[TYPE_NUM][Index[TYPE_NUM].size() - 1].Pointer = Data_t <T1>::Link.size() - 1;
+			// Global
+			TempG.Pointer = &Link[TYPE_NUM];
+			TempG.Index = Link[TYPE_NUM].size() - 1;
+
+			Data_t <T1>::Link.push_back(TempG);
+
+			// Update Local
+			Link[TYPE_NUM][Link[TYPE_NUM].size() - 1].Index = Data_t <T1>::Link.size() - 1;
 
 			AddPointer <T2, Args...>();
 		}
@@ -225,22 +209,36 @@ namespace SLGE
 		{
 			const int TYPE_NUM = 0;
 
-			//Erase pointer to Data
-			Data_t <T1>::Data.erase(Data_t <T1>::Data.begin() + Index[TYPE_NUM][in_Index].Data);
-			
-			//Shift local index to Data pointer down by one
-			for (int i = Index[TYPE_NUM][in_Index].Data; i < Data_t <T1>::Data.size(); i++)
-				Data_t <T1>::Index[Index[TYPE_NUM][in_Index].Data]--;
+		 	//std::vector	 Data_t <T1>::Link[Link[TYPE_NUM][i].Index].Index
 
-			//Shift local index to global index down by one
-			for (int i = Index[TYPE_NUM][in_Index].Pointer; i < Data_t <T1>::Index.size(); i++)
-				Data_t <T1>::Index[Index[TYPE_NUM][in_Index].Pointer]--;
+			//Shift all local indexes behind the deleted data back one
+			for (int i = 0; i < Data_t <T1>::Link.size(); i++)
+			{
+				// (*PointerToVec)[IndexOfVec].data >= IndexOfDataDeleted
+				if ((*Data_t <T1>::Link[i].Pointer)[Data_t <T1>::Link[i].Index].Data > Link[TYPE_NUM][in_Index].Data)
+					(*Data_t <T1>::Link[i].Pointer)[Data_t <T1>::Link[i].Index].Data--;
 
-			//Delete global index
-			Data_t <T1>::Index.erase(Data_t <T1>::Index.begin() + Index[TYPE_NUM][in_Index].Pointer);
-			
-			//Delete local index
-			Index[TYPE_NUM].erase(Index[TYPE_NUM].begin() + in_Index);
+				// (*PointerToVec)[IndexOfVec].index >= IndexOfIndexDeleted
+				if ((*Data_t <T1>::Link[i].Pointer)[Data_t <T1>::Link[i].Index].Index > Link[TYPE_NUM][in_Index].Index)
+					(*Data_t <T1>::Link[i].Pointer)[Data_t <T1>::Link[i].Index].Index--;
+			}
+
+			//Shift the global indexes behind the deleted data back one
+			for (int i = 0; i < Link[TYPE_NUM].size(); i++)
+			{
+				// Skip deleted index
+				if (i == in_Index)
+					continue;
+
+				// IndexToObjectIndex[IndexOfGlobal + 1] > IndexDeleted
+				if (Data_t <T1>::Link[Link[TYPE_NUM][i].Index + 1].Index > in_Index)
+					Data_t <T1>::Link[Link[TYPE_NUM][i].Index + 1].Index--;
+			}
+
+			//Delete Data						  
+			Data_t <T1>::Data.erase(Data_t <T1>::Data.begin() + Link[TYPE_NUM][in_Index].Data);
+			Data_t <T1>::Link.erase(Data_t <T1>::Link.begin() + Link[TYPE_NUM][in_Index].Index);
+			Link[TYPE_NUM].erase(Link[TYPE_NUM].begin());
 		}
 
 		template <typename T1, typename T2, typename ...Args>
@@ -248,22 +246,36 @@ namespace SLGE
 		{
 			const int TYPE_NUM = sizeof...(Args) + 1;
 
-			//Erase pointer to Data
-			Data_t <T1>::Data.erase(Data_t <T1>::Data.begin() + Index[TYPE_NUM][in_Index].Data);
-			
-			//Shift local index to Data pointer down by one
-			for (int i = Index[TYPE_NUM][in_Index].Data; i < Data_t <T1>::Data.size(); i++)
-				Data_t <T1>::Index[Index[TYPE_NUM][in_Index].Data]--;
+		 	//std::vector	 Data_t <T1>::Link[Link[TYPE_NUM][i].Index].Index
 
-			//Shift local index to global index down by one
-			for (int i = Index[TYPE_NUM][in_Index].Pointer; i < Data_t <T1>::Index.size(); i++)
-				Data_t <T1>::Index[Index[TYPE_NUM][in_Index].Pointer]--;
+			//Shift all local indexes behind the deleted data back one
+			for (int i = 0; i < Data_t <T1>::Link.size(); i++)
+			{
+				// (*PointerToVec)[IndexOfVec].data >= IndexOfDataDeleted
+				if ((*Data_t <T1>::Link[i].Pointer)[Data_t <T1>::Link[i].Index].Data > Link[TYPE_NUM][in_Index].Data)
+					(*Data_t <T1>::Link[i].Pointer)[Data_t <T1>::Link[i].Index].Data--;
 
-			//Delete global index
-			Data_t <T1>::Index.erase(Data_t <T1>::Index.begin() + Index[TYPE_NUM][in_Index].Pointer);
-			
-			//Delete local index
-			Index[TYPE_NUM].erase(Index[TYPE_NUM].begin() + in_Index);
+				// (*PointerToVec)[IndexOfVec].index >= IndexOfIndexDeleted
+				if ((*Data_t <T1>::Link[i].Pointer)[Data_t <T1>::Link[i].Index].Index > Link[TYPE_NUM][in_Index].Index)
+					(*Data_t <T1>::Link[i].Pointer)[Data_t <T1>::Link[i].Index].Index--;
+			}
+
+			//Shift the global indexes behind the deleted data back one
+			for (int i = 0; i < Link[TYPE_NUM].size(); i++)
+			{
+				// Skip deleted index
+				if (i == in_Index)
+					continue;
+
+				// IndexToObjectIndex[IndexOfGlobal + 1] > IndexDeleted
+				if (Data_t <T1>::Link[Link[TYPE_NUM][i].Index + 1].Index > in_Index)
+					Data_t <T1>::Link[Link[TYPE_NUM][i].Index + 1].Index--;
+			}
+
+			//Delete Data						  
+			Data_t <T1>::Data.erase(Data_t <T1>::Data.begin() + Link[TYPE_NUM][in_Index].Data);
+			Data_t <T1>::Link.erase(Data_t <T1>::Link.begin() + Link[TYPE_NUM][in_Index].Index);
+			Link[TYPE_NUM].erase(Link[TYPE_NUM].begin());
 
 			DeletePointer <T2, Args...>(in_Index);
 		}
@@ -272,17 +284,16 @@ namespace SLGE
 
 		DynamicClass()
 		{
-			Data.empty();
-
-			for (int i = 0; i < NUMBER_OF_TYPE_ARGS; i++)
-				Index[i].empty();
 		}
 
-		void Spawn(int Amount, Window_t &WindowHandle = NULL)
+		void Spawn(int Amount, Window_t *WindowHandle = nullptr)
 		{
 			for (int i = 0; i < Amount; i++)
 			{
-				Data.push_back(new C(&WindowHandle));
+				if (WindowHandle == nullptr)
+					Data.push_back(new C());
+				else
+					Data.push_back(new C(WindowHandle));
 
 				//Loop through the remaining class types
 				AddPointer <C, D...>();
@@ -291,17 +302,17 @@ namespace SLGE
 
 		void Despawn()
 		{
-			//Despawn(0, Data.size() - 1);
+			Despawn(0, Data.size() - 1);
 		}
 
 		void Despawn(int in_Index, int in_Amount)
 		{
 			for (int i = in_Index; i < in_Index + in_Amount; i++)
 			{
-				delete Data[i];
-				Data.erase(Data.begin() + i);
+				DeletePointer <C, D...>(in_Index);
 
-				DeletePointer <C, D...>(i);
+				delete Data[in_Index];
+				Data.erase(Data.begin() + in_Index);
 			}
 		}
 
@@ -313,16 +324,12 @@ namespace SLGE
 
 		static void All(vMethod Run)
 		{
-			const std::vector <C*> *DEBUG = &(Data_t <C>::Data);
-
 			for (int i = 0; i < (int) Data_t <C>::Data.size(); i++)
 				(Data_t <C>::Data[i]->*Run)();
 		}
 
 		static void All(iMethod Run)
 		{
-			const std::vector <C*> *DEBUG = &(Data_t <C>::Data);
-
 			for (int i = 0; i < (int) Data_t <C>::Data.size(); i++)
 				(Data_t <C>::Data[i]->*Run)();
 		}
@@ -348,198 +355,6 @@ namespace SLGE
 			return *(Data[0]);
 		}
 	};
-
-	/*
-	template <class C>
-	struct Tuple
-	{
-		std::vector <C*> Data;
-		std::vector <std::vector <int>*> Offset;
-		std::vector <int> Index;
-
-		Tuple()
-		{
-			Data.empty();
-			Offset.empty();
-			Index.empty();
-		}
-	};
-
-	//Used to consolidate the class lists between derived and base classes
-	template <class C>
-	class Data_t
-	{
-	public:
-		static Tuple <C> DynamicClass_ClassList;
-	};
-
-	template <class C> 
-	Tuple <C> Data_t <C>::DynamicClass_ClassList;
-
-	template <typename C, typename ...D>
-	class DynamicClass
-	{
-		typedef bool (C::*bMethod)();
-		typedef void (C::*vMethod)();
-		typedef int (C::*iMethod)();
-
-		std::vector <int> Index[sizeof...(D) +1];
-
-		template <typename T1>
-		void HiddenSpawn(int in_Index, int ClassIndex)
-		{
-			const Tuple <T1> *DATA = &(Data_t<T1>::DynamicClass_ClassList);
-
-			//Index[ClassIndex].push_back(Data_t <C>::DynamicClass_ClassList.Data.size());
-			//Data_t <C>::DynamicClass_ClassList.Offset.push_back(&Index[ClassIndex]);
-			//Data_t <C>::DynamicClass_ClassList.Index.push_back(Index[ClassIndex].size() - 1);
-									
-			Data_t <T1>::DynamicClass_ClassList.Data.push_back(Data_t<C>::DynamicClass_ClassList.Data[in_Index]);
-		}
-
-		template <typename T1, typename T2, typename ...Args>
-		void HiddenSpawn(int in_Index, int ClassIndex)
-		{
-			const Tuple <T1> *DATA = &(Data_t<T1>::DynamicClass_ClassList);
-
-			//Index[ClassIndex].push_back(Data_t <T1>::DynamicClass_ClassList.Data.size());
-			//Data_t <T1>::DynamicClass_ClassList.Offset.push_back(&Index[ClassIndex]);
-			//Data_t <T1>::DynamicClass_ClassList.Index.push_back(Index[ClassIndex].size() - 1);
-			
-			Data_t <T1>::DynamicClass_ClassList.Data.push_back(Data_t<C>::DynamicClass_ClassList.Data[in_Index]);
-
-			HiddenSpawn <T2, Args...>(in_Index, ClassIndex + 1);
-		}
-
-	public:
-
-		DynamicClass()
-		{
-			for (int i = 0; i < Index[0].size(); i++)
-				Index[i].empty();
-		}
-
-		void Spawn(int Amount, Window_t &WindowHandle = NULL)
-		{
-			for (int i = 0; i < Amount; i++)
-			{
-				const Tuple <C> *DATA = &(Data_t<C>::DynamicClass_ClassList);
-
-				//Hold the positions of the first pointer
-				//The first pointer is redundant since the actual data would be held in the same array
-				int Temp1 = Data_t <C>::DynamicClass_ClassList.Data.size();
-				int Temp2 = Data_t <C>::DynamicClass_ClassList.Offset.size();
-				int Temp3 = Data_t <C>::DynamicClass_ClassList.Index.size();
-
-				//Align the internal Index with the external, static data
-				Index[0].push_back(Data_t <C>::DynamicClass_ClassList.Data.size());
-
-				//Add a reference to the internal Index for later manipulation
-				Data_t <C>::DynamicClass_ClassList.Offset.push_back(&Index[0]);
-				Data_t <C>::DynamicClass_ClassList.Index.push_back(Index[0].size() - 1);
-
-				//Add the data
-				if (&WindowHandle == NULL)
-					Data_t <C>::DynamicClass_ClassList.Data.push_back(new C());
-				else
-					Data_t <C>::DynamicClass_ClassList.Data.push_back(new C(&WindowHandle));
-
-				//Loop through the remaining class types
-				HiddenSpawn <C, D...>(Temp1, 1);
-
-				//Remove redundant data and Index location information
-				Data_t <C>::DynamicClass_ClassList.Data.erase(Data_t <C>::DynamicClass_ClassList.Data.begin() + Temp1);
-				Data_t <C>::DynamicClass_ClassList.Offset.erase(Data_t <C>::DynamicClass_ClassList.Offset.begin() + Temp2);
-				Data_t <C>::DynamicClass_ClassList.Index.erase(Data_t <C>::DynamicClass_ClassList.Index.begin() + Temp3);
-			}
-		}
-
-		void Despawn()
-		{
-			while (Index[0].size() > 0)
-				Despawn(0);
-		}
-
-		void Despawn(int in_Index, int in_Amount = 1)
-		{
-			if (in_Index >= 0 && in_Amount > 0 && in_Index + in_Amount <= (int) Index[0].size())
-			{
-				for (int ii = 0; ii < in_Amount; ii++)
-				{
-					for (int iii = 0; iii < (sizeof...(D) + 1); iii++)
-					{
-						//Free data
-						delete Data_t <C>::DynamicClass_ClassList.Data[Index[iii][in_Index]];
-						Data_t <C>::DynamicClass_ClassList.Data[Index[iii][in_Index]] = nullptr;
-
-						//Shift table data
-						for (int i = 0; i < (int) Data_t <C>::DynamicClass_ClassList.Offset.size(); i++)
-						{
-							if (Data_t <C>::DynamicClass_ClassList.Offset[i]->at(Data_t <C>::DynamicClass_ClassList.Index[i]) > Index[iii][in_Index])
-								(Data_t <C>::DynamicClass_ClassList.Offset[i]->at(Data_t <C>::DynamicClass_ClassList.Index[i]))--;
-						}
-
-
-						//Delete table data
-						Data_t <C>::DynamicClass_ClassList.Data.erase(Data_t <C>::DynamicClass_ClassList.Data.begin() + Index[iii][in_Index]);
-						Data_t <C>::DynamicClass_ClassList.Offset.erase(Data_t <C>::DynamicClass_ClassList.Offset.begin() + Index[iii][in_Index]);
-						Data_t <C>::DynamicClass_ClassList.Index.erase(Data_t <C>::DynamicClass_ClassList.Index.begin() + Index[iii][in_Index]);
-						Index[iii].erase(Index[iii].begin() + in_Index);
-
-						//Update table Index
-						for (int i = 0; i < (int) Index[i].size(); i++)
-							Data_t <C>::DynamicClass_ClassList.Index[Index[iii][i]] = i;
-					}
-
-					in_Index++;
-				}
-			}
-		}
-
-		static void All(bMethod Run)
-		{
-			for (int i = 0; i < (int) Data_t <C>::DynamicClass_ClassList.Data.size(); i++)
-				(Data_t <C>::DynamicClass_ClassList.Data[i]->*Run)();
-		}
-
-		static void All(vMethod Run)
-		{
-			for (int i = 0; i < (int) Data_t <C>::DynamicClass_ClassList.Data.size(); i++)
-				(Data_t <C>::DynamicClass_ClassList.Data[i]->*Run)();
-		}
-
-		static void All(iMethod Run)
-		{
-			const Tuple <C> *DATA = &(Data_t<C>::DynamicClass_ClassList);
-
-			for (int i = 0; i < (int) Data_t <C>::DynamicClass_ClassList.Data.size(); i++)
-				(Data_t <C>::DynamicClass_ClassList.Data[i]->*Run)();
-		}
-
-		int NumberOfObjects()
-		{
-			return Index[0].size();
-		}
-
-		C &operator[](unsigned in_Index)
-		{
-			const Tuple <C> *DATA = &(Data_t<C>::DynamicClass_ClassList);
-
-			if (in_Index >= 0 && in_Index < Index[0].size())
-				return *(Data_t <C>::DynamicClass_ClassList.Data[Index[0][in_Index]]);
-		
-			return *(Data_t <C>::DynamicClass_ClassList.Data[Index[0][in_Index]]);
-		}
-
-		const C &operator[](unsigned in_Index) const
-		{
-			if (in_Index >= 0 && in_Index < Index[0].size())
-				return *(Data_t <C>::DynamicClass_ClassList.Data[Index[0][in_Index]]);
-		
-			return *(Data_t <C>::DynamicClass_ClassList.Data[Index[0][in_Index]]);
-		}
-	};
-	//*/
 
 	///^^^^^^^^^^^^^^^///
 	//	     WIP	   //
